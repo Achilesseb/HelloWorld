358cc7e36ce79beae0ab16e2dd2fd2ec8796e8f8
Prisacariu Victor-Sebastian <95617979+Achilesseb@users.noreply.github.com>
2023-08-16 14:37:53 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//using System;
+
+namespace Variables
+{
+    class Program { 
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 12.12f;
+            char character = 'v';
+            string firstName = "Victor";
+            bool isWorking = false;
+        }
+    }
+}
